dd944f39ea1818f18aefffe7c753e9a1673ad0d7
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-01 22:33:53 +0530

    Login

--- 08minicontext/src/App.jsx
+++ 08minicontext/src/App.jsx
@@ -1,14 +1,16 @@
 
 import './App.css'
+import UserContext from './context/UserContext'
+import UserContextProvider from './context/UserContextProvider'
 
 function App()
 {
   const [ count, setCount ] = useState( 0 )
 
   return (
-    <>
+    <UserContextProvider>
       <h1>Amit Context</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
