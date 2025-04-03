09af06a178a3eba2d149b844062973d876fc59fa
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-03 13:42:09 +0530

    context API

--- 08minicontext/src/App.jsx
+++ 08minicontext/src/App.jsx
@@ -1,22 +1,20 @@
-
-import './App.css'
-import Login from './components/Login'
-import Profile from './components/Profile'
-import UserContext from './context/UserContext'
-import UserContextProvider from './context/UserContextProvider'
+import React, { useState } from 'react';
+import './App.css';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import UserContextProvider from './context/UserContextProvider';
 
 function App()
 {
-  const [ count, setCount ] = useState( 0 )
+  const [ count, setCount ] = useState( 0 );
 
   return (
     <UserContextProvider>
       <h1>Amit Context</h1>
       <Login />
       <Profile />
-
     </UserContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
