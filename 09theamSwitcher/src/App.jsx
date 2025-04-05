d8b6f2368bf8c21b9f7dbe4f650755da4ec019ee
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-05 22:53:24 +0530

    theme switcher

--- 09theamSwitcher/src/App.jsx
+++ 09theamSwitcher/src/App.jsx
@@ -2,24 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App()
 {
   const [ count, setCount ] = useState( 0 )
 
   return (
+    <ThemeProvider>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/*Theam Button */}
+          </div>
 
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/*Theam Button */}
-        </div>
-
-        <div className="w-full max-w-sm mx-auto">
-          { /* Card*/}
+          <div className="w-full max-w-sm mx-auto">
+            { /* Card*/}
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
 
   )
 }
