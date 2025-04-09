488ac41b2351fcc0f50de246aff30da8401ad88c
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-09 17:49:34 +0530

    context API Part-2

--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,6 +1,17 @@
 import { createContext, useContext } from "react";
 
-export const TodoContext = createContext( {} )
+export const TodoContext = createContext( {
+    todos: [
+        {
+            id: 1,
+            todo: "Todo msg",
+            completed: false,
+        }
+    ],
+    theme: dark
+} )
+
+
 
 
 export const useTodo = () =>
